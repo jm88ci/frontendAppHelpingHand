ab2bdd9be6d6c6326d6dd9fe980486fc923d9c66
jm88ci <jm88ci@gmail.com>
2023-07-04 11:46:42 +0200

    Estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HomeModule} from "./home/home.module";
+import {PrimengModule} from "./primeng/primeng.module";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HomeModule,
+    PrimengModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    NavComponent,
+    BodyComponent,
+    FooterComponent
+  ],
+  exports: [
+    NavComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+  ],
   imports: [
     CommonModule
   ]
